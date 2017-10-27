27400d696843639869de12b9e19809a33e61b781
Sandro Pasquali <sandro.pasquali@twintechs.com>
2017-10-27 16:08:38 -0400

    better scroll behavior

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -5,17 +5,12 @@
     let $htmlbody = $('html,body');
 
     $(document).on('keydown', ev => {
-        if(ev.which === 40) {
-            scrollToNext();
-        } else if(ev.which === 38) {
-            scrollToPrev();
-        }
+        ev.which === 40 ? scrollTo(1) : ev.which === 38 ? scrollTo(-1) : 1;
     });
 
-    $('div#timeline_container').on('click','li', function(){
+    $('div#timeline_container').on('click','li', function() {
         showNext($(this));
     });
-
 
     $range
     .rangeslider({
@@ -32,39 +27,43 @@
             $spacing = $spacing || $('div#timeline_container > ul > li');
             $spacing.css('min-height', value);
         }
-    })
+    });
 
-    function showNext(li){
-        let $itms = $('div#timeline_container li');
-        $itms.removeClass('active');
-        $(li).addClass('active');
-        $htmlbody.stop().animate({ scrollTop: $(li).offset().top-$(li).height()}, 500,function(){
-            $htmlbody.stop();
-        });
+    // When item is clicked scroll it into view.
+    //
+    function showNext(li) {
+        let $items = $('div#timeline_container li');
+        let $item = $(li);
+        $items.removeClass('active');
+        $item.addClass('active');
+        $htmlbody.stop().animate({ scrollTop: $item.offset().top - $item.height()}, 500, $htmlbody.stop);
     }
 
-    function scrollToNext() {
-        let $itms = $('div#timeline_container li');
-        let $current=$itms.index($('div#timeline_container li.active'));
+    // Handler for up/down arrow scrolling
+    //
+    function scrollTo(delta) {
+        let $items = $('div#timeline_container li');
+        let current = $items.index($('div#timeline_container li.active'));
+        let idx = current + delta;
+        let _idx;
+        let $curr = $($items[idx]);
+        let len = $items.length;
 
-        if ($($itms[$current+1]).length>0 && !$($itms[$current+1]).hasClass('hidden')) {
-            $itms.removeClass('active');
-            $($itms[$current+1]).addClass('active');
-            $htmlbody.stop().animate({ scrollTop: $($itms[$current+1]).offset().top-$($itms[$current+1]).height()}, 500);
+        // If in bounds highlight, otherwise wrap and highlight first || last
+        //
+        if ((idx > 0) && (idx < len) && !$curr.hasClass('hidden')) {
+            $items.removeClass('active');
+            $curr.addClass('active');
+            $htmlbody.stop().animate({ scrollTop: $curr.offset().top - $curr.height()}, 500);
         } else {
-            $htmlbody.stop().animate({ scrollTop: $(document).height()}, 500);
-        }
-    }
-    function scrollToPrev() {
-        let $itms = $('div#timeline_container li');
-        let $current=$itms.index($('div#timeline_container li.active'));
-
-        if ($($itms[$current-1]).length>0 && !$($itms[$current-1]).hasClass('hidden')) {
-            $itms.removeClass('active');
-            $($itms[$current-1]).addClass('active');
-            $htmlbody.stop().animate({ scrollTop: $($itms[$current-1]).offset().top-$($itms[$current-1]).height()}, 500);
-        } else {
-            $htmlbody.stop().animate({ scrollTop: 0}, 500);
+            $items.removeClass('active');
+            _idx = (idx < 0) ? (len -1) : 0;
+            $($items[_idx]).addClass('active');
+            if(_idx === 0) {
+                $htmlbody.stop().animate({ scrollTop: 0}, 500);
+            } else {
+                $htmlbody.stop().animate({ scrollTop: $(document).height()}, 500);
+            }
         }
     }
 
